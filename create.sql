0d17720ee1280987010fababe0468e0089c9fcb5
Sai Prakash <prakashrayapati146@gmail.com>
2021-06-18 22:30:11 +0530

    Added required files for the assignment

---
+++ create.sql
@@ -0,0 +1,31 @@
+PRAGMA foreign_keys = ON;
+
+-- Creating publisher table
+
+create table publisher(
+    id integer primary key,
+    name text not null,
+    country text not null
+);
+
+-- Creating books table
+
+create table books(
+    id integer primary key,
+    title text not null,
+    publisher integer references publisher(id)
+);
+
+-- Creating subjects table
+
+create table subjects(
+    id integer primary key,
+    name text not null
+);
+
+-- Creating books_subjects table
+
+create table books_subjects(
+    book integer references books(id),
+    subject integer references subjects(id)
+);
